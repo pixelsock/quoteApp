350b8df83ac93762fe8ad4b3ef597b93fc475acd
pixelsock <nick@stump.works>
2024-04-18 15:54:52 -0400

    Publishing new release

--- src/generatePDF.js
+++ src/generatePDF.js
@@ -2,7 +2,7 @@ import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable'; 
 export { generatePDF };
 
-function generatePDF(quoteItems, finalTotal, totalFreight, quoteNumber, sku) {
+function generatePDF(quoteItems, finalTotal, quoteNumber, sku) {
     const pdf = new jsPDF({
         orientation: 'p',
         unit: 'pt',
@@ -10,7 +10,6 @@ function generatePDF(quoteItems, finalTotal, totalFreight, quoteNumber, sku) {
     });
 
     // Add the logo
-    // Assuming the logo is accessible at this path
     const logoUrl = 'https://uploads-ssl.webflow.com/638fbc9b6d164e234dc677d7/64e27f2a344c637a5c2038d4_logo.png'; // Replace with the path or base64 string of your logo
     pdf.addImage(logoUrl, 'PNG', 40, 30, 131, 45); // Adjust dimensions as needed
 
@@ -41,44 +40,40 @@ function generatePDF(quoteItems, finalTotal, totalFreight, quoteNumber, sku) {
     pdf.setDrawColor(0, 0, 0); // Set draw color to black
     pdf.line(40, 140, pdf.internal.pageSize.width - 40, 140); // Draw a line from left to right
 
-    // Add the table header and items with added DESCRIPTION and FREIGHT
+    // Add the table header and items without FREIGHT
     autoTable(pdf, {
         startY: 150,
         styles: {
             fontSize: 10, // Default font size for the table
             cellPadding: 5,
         },
-       // make the table width the same size and the pdf.line above
         columnStyles: {
             0: { cellWidth: 140 }, // ITEM NUMBER
-            1: { cellWidth: 140 }, // DESCRIPTION
+            1: { cellWidth: 180 }, // DESCRIPTION
             2: { cellWidth: 40 }, // QTY
             3: { cellWidth: 70 }, // BASE
-            4: { cellWidth: 70 }, // FREIGHT
-            5: { cellWidth: 70 }, // TOTAL COST
+            4: { cellWidth: 70 }, // TOTAL COST
         },
         headStyles: {
             fillColor: [211, 211, 211], // A light gray background for the header
             textColor: [0, 0, 0], // Black text for the header
         },
-        head: [['ITEM NUMBER', 'DESCRIPTION', 'QTY', 'BASE', 'FREIGHT', 'COST']],
+        head: [['ITEM NUMBER', 'DESCRIPTION', 'QTY', 'BASE', 'COST']],
         body: quoteItems.map(item => [
             item.product,
             item.description,
             item.quantity, 
-            `$${(Number(item.total_price) / item.quantity || 0).toFixed(2)}`, // Renamed to 'BASE'
-            `$${(Number(item.freight) || 0).toFixed(2)}`,
-            `$${(Number(item.total_price) || 0).toFixed(2)}`
+            `$${(Number(item.total_price) / item.quantity || 0).toFixed(2)}`, // BASE
+            `$${(Number(item.total_price) || 0).toFixed(2)}` // TOTAL COST
         ]),
         footStyles: {
             fillColor: [4, 7, 7], // A dark gray background for the footer
-
             textColor: [255, 255, 255], // White text
             fontSize: 10, // Slightly larger text for the footer
             cellPadding: 10, // Increased height for the footer
         },
         foot: [
-            ['TOTALS','', '', '', `$${totalFreight.toFixed(2)}`, `$${(finalTotal + totalFreight).toFixed(2)}`]
+            ['TOTALS','', '', '', `$${finalTotal.toFixed(2)}`]
         ]
     });
 
