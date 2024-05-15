4132c9d6c70e3409cbf1a5160577b0eec8686666
pixelsock <nick@stump.works>
2024-05-15 10:32:17 -0400

    Remove hardcoded secrets

--- webpack.config.js
+++ webpack.config.js
@@ -34,5 +34,15 @@ module.exports = {
     new webpack.IgnorePlugin({ resourceRegExp: /^\.\/working_files\// }),
     new webpack.HotModuleReplacementPlugin(),
   ],
+  resolve: {
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify"),
+      "buffer": require.resolve("buffer/"),
+      "vm": require.resolve("vm-browserify"),
+      "stream": require.resolve("stream-browserify")
+    }
+  },
   // Add your other webpack configurations here
 };
