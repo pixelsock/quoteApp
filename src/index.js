e4cd1ca6187f40467e60c7566069835b5bd42a38
pixelsock <nick@stump.works>
2024-06-11 14:42:23 -0400

    Commit all changes

--- src/index.js
+++ src/index.js
@@ -625,7 +625,6 @@ function updateTotals() {
     }
 }
 
-alert('Hello World');
 
 // Example implementations of the functions to gather data (you'll need to adjust these based on your actual data structure)
 function getQuoteItems() {
